05ca17714d35a7c736a5a3412efde4f1c03edfab
praneethbittu159-cmd <praneethbittu159@gmail.com>
2026-07-20 08:46:33 +0530

    DAA

---
+++ BubbleSort.py
@@ -0,0 +1,19 @@
+#sorting Algorithm
+#Bubble sort
+#time complexity
+#Best case:o(n)
+#Average case:o(n^2)
+#worst case:o(n^2)
+#Space complexity:o(1)
+def BubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+      for j in range(0, n-i-1):
+        if arr[j]>arr[j+1]:
+          arr[j],arr[j+1]=arr[j+1],arr[j]
+arr=[64,34,25,12,22,11,90]
+
+BubbleSort(arr)
+
+print("sorted Array:")
+print(arr)
